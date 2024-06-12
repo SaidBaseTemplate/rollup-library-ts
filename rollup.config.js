15d0cc4999da3d850ed88beae3e615a09dc7e1d4
guizimo <17680262548@163.com>
2024-06-12 23:49:59 +0800

    feat：调整配置

---
+++ rollup.config.js
@@ -0,0 +1,20 @@
+import typescript from 'rollup-plugin-typescript2'; // 处理typescript
+import babel from '@rollup/plugin-babel';
+
+export default [
+    {
+        input: 'src/index.ts',
+        plugins: [
+            typescript(), // typescript 转义
+            babel({
+                babelrc: false,
+                presets: [['@babel/preset-env', { modules: false, loose: true }]],
+                plugins: [['@babel/plugin-proposal-class-properties', { loose: true }]],
+                exclude: 'node_modules/**',
+            })
+        ],
+        output: [
+            { file: 'bin/www.js', format: 'cjs' },
+        ]
+    }
+];
