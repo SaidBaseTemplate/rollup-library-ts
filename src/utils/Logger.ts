9f18377f77cf999dab0b1c42b7f9fbd79936d926
guizimo <17680262548@163.com>
2024-06-26 15:22:01 +0800

    feat(Logger): 优化Logger类

--- src/utils/Logger.ts
+++ src/utils/Logger.ts
@@ -9,19 +9,30 @@ import chalk from 'chalk';
 const chalkLog = (type: string, msg: string, bold: boolean) => {
   let color = 'yellow';
   if (type === 'success') color = 'green';
+  else if (type === 'info') color = 'blue';
   else if (type === 'error') color = 'red';
   // @ts-ignore
   const handler = bold ? chalk.bold[color](msg) : chalk[color](msg);
   return console.log(handler);
 };
 
+/**
+ * 日志类
+ */
 export default class Logger {
+  // 成功
   success(msg: string, bold: boolean = false) {
     return chalkLog('success', msg, bold);
   }
+  // 输出
+  info(msg: string, bold: boolean = false) {
+    return chalkLog('info', msg, bold);
+  }
+  // 警告
   warn(msg: string, bold: boolean = false) {
     return chalkLog('warn', msg, bold);
   }
+  // 错误
   error(msg: string, bold: boolean = false) {
     return chalkLog('error', msg, bold);
   }
